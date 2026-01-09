3d62db124fd2eb721e0e4198f484f2b5a9cf16c2
kajyrkka <kari.jyrkka@oamk.fi>
2026-01-09 11:15:27 +0200

    ekaCPPohjelma githubiin

---
+++ Week1/ekaCppOhjelma/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    cout << "int muuttujan koko byteina = "<<sizeof(int) << endl;
+    cout << "long muuttujan koko byteina = "<<sizeof(long) << endl;
+    cout << "long long muuttujan koko byteina = "<<sizeof(long long) << endl;
+    return 0;
+}
